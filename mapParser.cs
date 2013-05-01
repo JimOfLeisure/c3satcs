10c84b5f5ed43ab8c192bec081b4d1894559c7fd
Jim <Jim@dff72d60-8219-4f40-8fb8-21f5c4b7036c>
2013-05-01 05:30:35 +0000

    Switched back to BinaryReader for its methods to read directly into data types. This successfully prints the first four bytes "CIV3" using BinaryReader
    git-svn-id: file:///c/Users/jim.AD/Documents/svn_repo/c3satcs@4 dff72d60-8219-4f40-8fb8-21f5c4b7036c

--- mapParser.cs
+++ mapParser.cs
@@ -16,9 +16,18 @@ class KickItOff {
 		// FIX: Hard-coding the save file name to my test save in the current folder
 		string SaveFilePath = "unc-test.sav";
 
+/* Trying BinaryReader instead of File.ReadAllBytes
 		// Read the file into a byte array.
 		byte[] SaveFileByteArray = File.ReadAllBytes(SaveFilePath);
 		System.Text.ASCIIEncoding ascii = new System.Text.ASCIIEncoding();
 		Console.WriteLine(ascii.GetString(SaveFileByteArray, 0, 4));
+*/
+
+		// Open the binary file.
+		// Does the 'using' statement do anything for me here? Seems to crash if the file doesn't exist.
+		using (BinaryReader SaveStream = new BinaryReader(File.Open(SaveFilePath,FileMode.Open))) {
+			Console.WriteLine("Inside the using file statement");
+			Console.WriteLine(SaveStream.ReadChars(4));
+		}
 	}
 }
